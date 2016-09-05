0bf0512651732f59cbf176a05381110fd6d9804a
Joost <joostvherwaarden@gmail.com>
2016-09-05 14:05:32 +0200

    Added the following: -Different styling for main form (background, textboxes and buttons) -Register button on-click event which opens a new register form -Register form can be filled in. After pressing the button an account will be added to the SQL Database

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_1___Pokemon_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(@"C:\Users\joost\Documents\visual studio 2015\Projects\Project 1 - Pokemon WPF\Afbeeldingen\inlog.jpg")));
+        }
+
+        private void txt_username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_username.Clear();
+        }
+
+        private void txt_password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_password.Clear();
+        }
+
+        private void btn_register_Click(object sender, RoutedEventArgs e)
+        {
+            Register register_form = new Register();
+            register_form.Show();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Register.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Project_1___Pokemon_WPF
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string constring = "datasource=127.0.0.1; port=3307; username=root; password=usbw;";
+            string Query = "INSERT INTO pokemon_db.users(Username, Password, Mail)VALUES('" + txt_username.Text + "','" + txt_password.Text + "','" + txt_mail.Text + "') ; ";
+            MySqlConnection conDatabase = new MySqlConnection(constring);
+            MySqlCommand cmdDatabase = new MySqlCommand(Query, conDatabase);
+            MySqlDataReader myReader;
+
+            try
+            {
+
+                conDatabase.Open();
+                myReader = cmdDatabase.ExecuteReader();
+                MessageBox.Show("Gebruiker aangemaakt!");
+                while (myReader.Read())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
