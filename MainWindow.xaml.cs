1e9999fd8cf94cae0e434e3db29d30ad58825cf2
Joost <joostvherwaarden@gmail.com>
2016-09-05 16:15:28 +0200

    Changed the following things: -Not using form2 anymore for registering. It just hides/shows components to make it look smoother (same screen) -Checks on registering for username and password (8-characters check and if the values aren't default)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Project_1___Pokemon_WPF
 {
@@ -24,6 +25,12 @@ namespace Project_1___Pokemon_WPF
         {
             InitializeComponent();
             this.Background = new ImageBrush(new BitmapImage(new Uri(@"C:\Users\joost\Documents\visual studio 2015\Projects\Project 1 - Pokemon WPF\Afbeeldingen\inlog.jpg")));
+
+            //All components that needs to be hidden at start up.
+            screen_register.Height = 0;
+            btn_registerscreen_back.Height = 0;
+            btn_registerscreen_register.Height = 0;
+            txt_mail.Height = 0;
         }
 
         private void txt_username_GotFocus(object sender, RoutedEventArgs e)
@@ -38,13 +45,98 @@ namespace Project_1___Pokemon_WPF
 
         private void btn_register_Click(object sender, RoutedEventArgs e)
         {
-            Register register_form = new Register();
-            register_form.Show();
+            //Register register_form = new Register(); (show form 2: Register.xaml. Isn't needed anymore as we do that in this code by showing/hiding components)
+            //register_form.Show();
+
+            screen_login.Height = 0;
+            screen_register.Height = 320;
+            btn_registerscreen_register.Height = 50;
+            btn_registerscreen_back.Height = 50;
+            btn_register.Height = 0;
+            btn_login.Height = 0;
+            btn_forgotPassword.Height = 0;
+            txt_mail.Height = 44;
+            txt_username.Text = "Username";
+            txt_password.Text = "Password";
+            txt_mail.Text = "Mail";
+        }
+
+        private void btn_registerscreen_back_Click(object sender, RoutedEventArgs e)
+        {
+            screen_register.Height = 0;
+            screen_login.Height = 250;
+            btn_registerscreen_register.Height = 0;
+            btn_registerscreen_back.Height = 0;
+            btn_register.Height = 50;
+            btn_login.Height = 50;
+            btn_forgotPassword.Height = 50;
+            txt_mail.Height = 0;
+            txt_username.Text = "Username";
+            txt_password.Text = "Password";
+        }
+        private void btn_registerscreen_register_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_username.Text.Length > 7 || txt_password.Text.Length > 8)
+            {
+                if (txt_username.Text != "Username" || txt_password.Text != "Password")
+                {
+                    string constring = "datasource=127.0.0.1; port=3307; username=root; password=usbw;";
+                    string Query = "INSERT INTO pokemon_db.users(Username, Password, Mail)VALUES('" + txt_username.Text + "','" + txt_password.Text + "','" + txt_mail.Text + "') ; ";
+                    MySqlConnection conDatabase = new MySqlConnection(constring);
+                    MySqlCommand cmdDatabase = new MySqlCommand(Query, conDatabase);
+                    MySqlDataReader myReader;
+
+                    try
+                    {
+
+                        conDatabase.Open();
+                        myReader = cmdDatabase.ExecuteReader();
+                        MessageBox.Show("Gebruiker aangemaakt!");
+                        txt_password.Clear();
+                        txt_username.Clear();
+                        txt_mail.Clear();
+                        while (myReader.Read())
+
+                        screen_register.Height = 0;
+                        screen_register.Width = 0;
+                        screen_login.Height = 250;
+                        btn_registerscreen_register.Height = 0;
+                        btn_registerscreen_back.Height = 0;
+                        btn_register.Height = 50;
+                        btn_login.Height = 50;
+                        btn_forgotPassword.Height = 50;
+                        txt_mail.Height = 0;
+                        txt_username.Text = "Username";
+                        txt_password.Text = "Password";
+
+                        {
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vergeet natuurlijk niet je eigen gegevens te verzinnen.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vul bij zowel gebruikersnaam als wachtwoord minimaal 8 tekens in.");
+            }
         }
 
         private void btn_login_Click(object sender, RoutedEventArgs e)
         {
+            throw new NotImplementedException();
+        }
 
+        private void txt_mail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_mail.Clear();
         }
     }
 }
--- Register.xaml.cs
+++ Register.xaml.cs
@@ -23,32 +23,67 @@ namespace Project_1___Pokemon_WPF
         public Register()
         {
             InitializeComponent();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(@"C:\Users\joost\Documents\visual studio 2015\Projects\Project 1 - Pokemon WPF\Afbeeldingen\register.jpg")));
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            string constring = "datasource=127.0.0.1; port=3307; username=root; password=usbw;";
-            string Query = "INSERT INTO pokemon_db.users(Username, Password, Mail)VALUES('" + txt_username.Text + "','" + txt_password.Text + "','" + txt_mail.Text + "') ; ";
-            MySqlConnection conDatabase = new MySqlConnection(constring);
-            MySqlCommand cmdDatabase = new MySqlCommand(Query, conDatabase);
-            MySqlDataReader myReader;
-
-            try
-            {
-
-                conDatabase.Open();
-                myReader = cmdDatabase.ExecuteReader();
-                MessageBox.Show("Gebruiker aangemaakt!");
-                while (myReader.Read())
+            if (txt_username.Text.Length > 7 || txt_password.Text.Length > 8) {
+                if (txt_username.Text != "Username" || txt_password.Text != "Password")
                 {
+                    string constring = "datasource=127.0.0.1; port=3307; username=root; password=usbw;";
+                    string Query = "INSERT INTO pokemon_db.users(Username, Password, Mail)VALUES('" + txt_username.Text + "','" + txt_password.Text + "','" + txt_mail.Text + "') ; ";
+                    MySqlConnection conDatabase = new MySqlConnection(constring);
+                    MySqlCommand cmdDatabase = new MySqlCommand(Query, conDatabase);
+                    MySqlDataReader myReader;
 
+                    try
+                    {
+
+                        conDatabase.Open();
+                        myReader = cmdDatabase.ExecuteReader();
+                        MessageBox.Show("Gebruiker aangemaakt!");
+                        txt_password.Clear();
+                        txt_username.Clear();
+                        txt_mail.Clear();
+                        while (myReader.Read())
+                        {
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                } else
+                {
+                    MessageBox.Show("Vergeet natuurlijk niet je eigen gegevens te verzinnen.");
                 }
-            }
-            catch (Exception ex)
+            } else
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Vul bij zowel gebruikersnaam als wachtwoord minimaal 8 tekens in.");
             }
-            
+
+        }
+
+        private void txt_username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_username.Clear();
+        }
+
+        private void txt_password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_password.Clear();
+        }
+
+        private void txt_mail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_mail.Clear();
+        }
+
+        private void btn_registerscreen_register_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
