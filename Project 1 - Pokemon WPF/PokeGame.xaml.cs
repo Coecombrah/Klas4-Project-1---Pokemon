74d3203c608644b6a466a5a695e063c6d823de68
Joost <joostvherwaarden@gmail.com>
2016-10-04 16:04:30 +0200

    Added an image that randomly gets displayed in a canvas

--- Project 1 - Pokemon WPF/PokeGame.xaml.cs
+++ Project 1 - Pokemon WPF/PokeGame.xaml.cs
@@ -19,12 +19,17 @@ namespace Project_1___Pokemon_WPF
     /// </summary>
     public partial class PokeGame : Window
     {
+        BitmapImage carBitmap = new BitmapImage(new Uri("pack://application:,,,/Images/download.png", UriKind.Absolute));
+        Image[] carImg = new Image[5];
+        Random rnd = new Random();
 
         public PokeGame(string userLoggedName, double userLogged)
             //userLoggedName = the name that comes from login screen
             //userLogged should be "1". Stating that there's a user logged in. To log out we can change this back to "0".
-        {
+
             
+        {
+
             InitializeComponent();
             this.Background = new ImageBrush(new BitmapImage(new Uri(@"C:\Users\joost\Documents\visual studio 2015\Projects\Project 1 - Pokemon WPF\Afbeeldingen\PokeGame_background.png")));
             lbl_welkom.Content = "Welcome, " + userLoggedName; //Left upper corner shows welcome, <name> to see who's logged in.
@@ -33,7 +38,22 @@ namespace Project_1___Pokemon_WPF
             } else {
                 lbl_ingelogd.Content = "Not logged in";
             }
-            string vogelbeer = userLoggedName;
+
+
+            double maxX = canvas1.Width - carBitmap.Width;
+            double maxY = canvas1.Height - carBitmap.Height;
+            for (int i = 0; i < carImg.Length; i++)
+            {
+                carImg[i] = new Image();
+                carImg[i].Source = carBitmap;
+                carImg[i].Width = carBitmap.Width;
+                carImg[i].Height = carBitmap.Height;
+                Canvas.SetLeft(carImg[i], rnd.NextDouble() * maxX);
+                Canvas.SetTop(carImg[i], rnd.NextDouble() * maxY);
+                canvas1.Children.Add(carImg[i]);
+            }
+
+
         }
         
         public void MenuItem_Click_LogOff(object sender, RoutedEventArgs e)
